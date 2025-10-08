ece9a3d8d372c8e3c3bbf4ab5abb85d2234f137e
William Chastain <wbc008@bucknell.edu>
2025-10-08 00:20:25 -0400

    Update main.go

--- main.go
+++ main.go
@@ -18,9 +18,6 @@ import (
 	"key/config"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
 	// Load config
 	cfg, err := config.Load()
